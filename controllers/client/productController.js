fc523db141fb0195c31efa47451b9f944e409ac7
Nguyendinhquan2605 <dinhquana12@gmail.com>
2025-02-26 23:56:29 +0700

    set up folder View-pug

--- controllers/client/productController.js
+++ controllers/client/productController.js
@@ -1,5 +1,7 @@
 module.exports.index = (req, res) => {
-  res.render("client/page/products/index.pug");
+  res.render("client/page/products/index.pug", {
+    pageTitle: "Danh sách sản phẩm",
+  });
 };
 
 module.exports.edit = (req, res) => {
