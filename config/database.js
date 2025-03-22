ffa2c19a03f58b1c14354eb5c99c23842b8d5863
Nguyendinhquan2605 <dinhquana12@gmail.com>
2025-03-23 00:12:09 +0700

    Config database mongoDB Atlas

--- config/database.js
+++ config/database.js
@@ -1,35 +1,28 @@
 const mongoose = require("mongoose");
+
 const dbState = [
-  {
-    value: 0,
-    label: "disconnected",
-  },
-  {
-    value: 1,
-    label: "connected",
-  },
-  {
-    value: 2,
-    label: "connecting",
-  },
-  {
-    value: 3,
-    label: "disconnecting",
-  },
+  { value: 0, label: "disconnected" },
+  { value: 1, label: "connected" },
+  { value: 2, label: "connecting" },
+  { value: 3, label: "disconnecting" },
 ];
 
 const connection = async () => {
   try {
-    const option = {
-      user: "root",
-      pass: "123456",
-      dbName: "project-test",
+    const MONGO_URI =
+      "mongodb+srv://ndquan2605:0904663861qq@cluster0.4cqy2.mongodb.net/Project_Test?retryWrites=true&w=majority";
+
+    const options = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     };
-    await mongoose.connect("mongodb://localhost:27018/", option);
-    const state = Number(mongoose.connection.readyState);
-    console.log(dbState.find((f) => f.value == state).label, "to db"); // connected to db
+
+    await mongoose.connect(MONGO_URI, options);
+
+    const state = mongoose.connection.readyState;
+    console.log(dbState.find((f) => f.value === state).label, "to db ✅");
   } catch (error) {
-    console.log(">>> Error connection DB: ", error);
+    console.log(">>> Error connecting to MongoDB:", error);
   }
 };
 
