9e7b39e0416d10b508d3ebc730dc4edef8d4fe3b
Nguyendinhquan2605 <dinhquana12@gmail.com>
2025-03-30 23:38:23 +0700

    Upload Image Tiny-MCE

--- controllers/admin/products_category.js
+++ controllers/admin/products_category.js
@@ -6,12 +6,14 @@ module.exports.index = async (req, res) => {
     deleted: false,
   };
 
+  let count = 0;
   function createTree(arr, parentId = "") {
     const tree = [];
-
     arr.forEach((item) => {
       if (item.parent_id === parentId) {
+        count++;
         const newItem = item;
+        newItem.index = count;
         const children = createTree(arr, item.id);
         if (children.length > 0) {
           newItem.children = children;
