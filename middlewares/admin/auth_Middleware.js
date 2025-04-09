e119564e97ef7d1c1f5ccdb06fb60566cf7b4701
Nguyendinhquan2605 <dinhquana12@gmail.com>
2025-04-10 00:51:44 +0700

    complete the authorization function

--- middlewares/admin/auth_Middleware.js
+++ middlewares/admin/auth_Middleware.js
@@ -1,17 +1,23 @@
 const Accounts = require("../../models/account_model");
+const Roles = require("../../models/roles_Model");
 
 module.exports.requireAuth = async (req, res, next) => {
-  console.log(req.cookies.token);
+  // console.log(req.cookies.token);
   if (!req.cookies.token) {
     res.redirect("/admin/auth/login");
   } else {
     const user = await Accounts.findOne({
       token: req.cookies.token,
-    });
+    }).select("-password");
     if (!user) {
       res.redirect("/admin/auth/login");
     } else {
-      console.log("check user: ", user);
+      const role = await Roles.findOne({
+        _id: user.role_id,
+      }).select(" title permissions");
+
+      res.locals.user = user;
+      res.locals.role = role;
       next();
     }
   }
