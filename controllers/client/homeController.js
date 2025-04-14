2270fb1288d3569045ea96f60f54ad203b6eb96e
Nguyendinhquan2605 <dinhquana12@gmail.com>
2025-04-15 01:27:06 +0700

    Show new  products on home page

--- controllers/client/homeController.js
+++ controllers/client/homeController.js
@@ -2,18 +2,31 @@ const Product = require("../../models/products_models");
 const products_helper = require("../../helper/products");
 
 module.exports.index = async (req, res) => {
-  // console.log(">>>check: ", newProductCategory);
+  //Lay ra an pham not bat
   const products_Prominent = await Product.find({
     deleted: false,
     featured: "1",
     status: "active",
   }).limit(4);
 
-  // console.log(">>>check products_Prominent: ", products_Prominent);
-  const newProducts = products_helper.PriceNew_Products(products_Prominent);
+  const newProducts_Featured =
+    products_helper.PriceNew_Products(products_Prominent);
+  //Lay ra an pham not bat
+
+  //Lay ra san pham moi nhat
+  const Products_new = await Product.find({
+    deleted: false,
+    status: "active",
+  })
+    .sort({ position: "desc" })
+    .limit(6);
+
+  const newProducts_new = products_helper.PriceNew_Products(Products_new);
+  //Lay ra san pham moi nhat
 
   res.render("client/page/home/index.pug", {
     pageTitle: "Trang chủ",
-    products_Featured: newProducts,
+    products_Featured: newProducts_Featured,
+    products_new: newProducts_new,
   });
 };
