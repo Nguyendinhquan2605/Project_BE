4b4b6335b744bdfaec1ebdc06e6fe54ea669c035
Nguyendinhquan2605 <dinhquana12@gmail.com>
2025-04-23 00:51:40 +0700

    Collection Users

---
+++ models/user_model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const generate = require("../helper/generate");
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: String,
+    email: String,
+    password: String,
+    tokenUser: {
+      type: String,
+      default: generate.generateRandomString(20),
+    },
+    phone: String,
+    avatar: String,
+    status: {
+      type: String,
+      default: "active",
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+    deleteAt: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Override all methods
+
+const User = mongoose.model("User", userSchema, "user");
+
+module.exports = User;
